a36f0907a6c8fefd12d9fa727c998942873dd717
Sri Pusponegoro <puspo@team-company.asia>
2018-11-08 00:58:55 +0700

    Add scopes query

--- app/Model/HomeBanner.php
+++ app/Model/HomeBanner.php
@@ -3,8 +3,18 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class HomeBanner extends Model
 {
     protected $table = 'cms_tm_fe_homebanner';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('_active', function (Builder $builder) {
+            $builder->where('_active', '=', '1');
+        });
+    }
 }
