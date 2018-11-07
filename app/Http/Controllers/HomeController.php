e51b699e1603e23098005886d033d564e48c2f9d
Sri Pusponegoro <puspo@team-company.asia>
2018-11-07 22:21:52 +0700

    update banner

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\HomeBanner;
 
 class HomeController extends Controller
 {
@@ -13,6 +14,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home.index');
+        $home_banner = HomeBanner::orderBy('created_at','desc')->get();
+
+        return view('home.index', compact('home_banner'));
     }
 }
---
+++ app/Model/HomeBanner.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HomeBanner extends Model
+{
+    protected $table = 'cms_tm_fe_homebanner';
+}
