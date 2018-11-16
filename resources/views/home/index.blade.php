79146eef583c877733bf420699cd742373a9bba6
kevin <kahal.kevin@gmail.com>
2018-11-16 08:53:08 +0700

    home product showcase

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,9 +8,9 @@
       <div class="slider-activation owl-carousel">
         <!-- Start Single Slide -->
         @foreach($home_banners as $banner)
-          <a href="{{ $banner['_href_url'] }}">
+          <a href="{{ $banner->_href_url }}">
             <div class="slide align-center-left fullscreen animation-style-01">
-              <img src="{{ getenv('IMG_URL_PREFIX') . $banner['_image_url'] }}" alt="">
+              <img src="{{ $cmsUrl . $banner->_image_url }}" alt="">
             </div>
           </a>
         @endforeach
@@ -29,99 +29,28 @@
         </div>
         <!-- Section Title End Here -->
         <br>
-        <div class="row">
-          <!-- Single Service Area Start Here -->
-          <div class="col-lg-3 mb-all-40">
-            <div class="single-elomous-product">
-              <span class="new-notif">New</span>
-              <!-- Product Image Start -->
-              <div class="pro-img">
-                <a href="shop/detail">
-                  <img class="primary-img" src="./img/product/nike-mercurial.png" alt="single-product">
-                </a>
-              </div>
-              <!-- Product Image End -->
-              <!-- Product Content Start -->
-              <div class="pro-content">
-                <div class="pro-info">
-                  <h4><a href="shop/detail">Sepatu Futsal Nike Mercurial</a></h4>
-                  <p><span class="special-price">Rp. 35.000</span></p>
-                </div>
-              </div>
-              <!-- Product Content End -->
-            </div>
-          </div>
-          <!-- Single Service Area End Here -->
-          <!-- Single Service Area Start Here -->
+        <div class="row">  
+        @foreach($latest_products as $pr)
           <div class="col-lg-3 mb-all-40">
             <div class="single-elomous-product">
               <!-- Product Image Start -->
               <div class="pro-img">
                 <a href="shop/detail">
-                  <img class="primary-img" src="./img/product/converse-tshirt2.png" alt="single-product">
+                  <img class="primary-img" src="{{$cmsUrl . $pr->_image_url }}" alt="single-product">
                 </a>
               </div>
               <!-- Product Image End -->
               <!-- Product Content Start -->
               <div class="pro-content">
                 <div class="pro-info">
-                  <h4><a href="shop/detail">T-Shirt Converse White</a></h4>
-                  <p><span class="special-price">Rp. 150.000</span></p>
-                  <!-- <del class="old-price">$80.50</del> -->
+                  <h4><a href="shop/detail">{{ $pr->_name }}</a></h4>
+                  <p><span class="special-price">Rp. {{ number_format($pr->_price, 2) }}</span></p>
                 </div>
               </div>
               <!-- Product Content End -->
-              <!-- <span class="sticker-sale">-5%</span> -->
             </div>
           </div>
-          <!-- Single Service Area End Here -->
-          <!-- Single Service Area Start Here -->
-          <div class="col-lg-3 mb-sm-40">
-            <div class="single-elomous-product">
-              <!-- Product Image Start -->
-              <span class="new-notif">50%</span>
-              <div class="pro-img">
-                <a href="shop/detail">
-                  <img class="primary-img" src="./img/product/arsenal.png" alt="single-product">
-                </a>
-              </div>
-              <!-- Product Image End -->
-              <!-- Product Content Start -->
-              <div class="pro-content">
-                <div class="pro-info">
-                  <h4><a href="shop/detail">Jersey Puma Arsenal 2018/2019</a></h4>
-                  <p><span class="special-price">Rp. 850.000</span></p>
-                  <!-- <del class="old-price">$80.50</del> -->
-                </div>
-              </div>
-              <!-- Product Content End -->
-              <!-- <span class="sticker-sale">-5%</span> -->
-            </div>
-          </div>
-          <!-- Single Service Area End Here -->
-          <!-- Single Service Area Stat Here -->
-          <div class="col-lg-3 ">
-            <div class="single-elomous-product">
-              <!-- Product Image Start -->
-              <div class="pro-img">
-                <a href="shop/detail">
-                  <img class="primary-img" src="./img/product/adidas-gazelle2.png" alt="single-product">
-                </a>
-              </div>
-              <!-- Product Image End -->
-              <!-- Product Content Start -->
-              <div class="pro-content">
-                <div class="pro-info">
-                  <h4><a href="shop/detail">Adidas Gazelle II Orange</a></h4>
-                  <p><span class="special-price">Rp. 500.000</span></p>
-                  <!-- <del class="old-price">$80.50</del> -->
-                </div>
-              </div>
-              <!-- Product Content End -->
-              <!-- <span class="sticker-sale">-5%</span> -->
-            </div>
-          </div>
-          <!-- Single Service Area End Here -->
+        @endforeach
         </div>
         <!-- Big drone Image Area Start -->
         <div class="dron-img opacity-img mt-80">
@@ -145,21 +74,22 @@
         <!-- Section Title End Here -->
         <br>
         <div class="row">
+        @foreach($best_products as $pr)
           <!-- Single Service Area Start Here -->
           <div class="col-lg-3  mb-all-40">
             <div class="single-elomous-product">
               <!-- Product Image Start -->
               <div class="pro-img">
                 <a href="shop/detail">
-                  <img class="primary-img" src="./img/product/nb.png" alt="single-product">
+                  <img class="primary-img" src="{{$cmsUrl . $pr->_image_url }}" alt="single-product">
                 </a>
               </div>
               <!-- Product Image End -->
               <!-- Product Content Start -->
               <div class="pro-content">
                 <div class="pro-info">
-                  <h4><a href="shop/detail">New Balance Running</a></h4>
-                  <p><span class="special-price">Rp. 350.000</span></p>
+                  <h4><a href="shop/detail">{{ $pr->_name }}</a></h4>
+                  <p><span class="special-price">Rp. {{ number_format($pr->_price, 2) }}</span></p>
                   <!-- <del class="old-price">$80.50</del> -->
                 </div>
               </div>
@@ -168,74 +98,7 @@
             </div>
           </div>
           <!-- Single Service Area End Here -->
-          <!-- Single Service Area Start Here -->
-          <div class="col-lg-3  mb-all-40">
-            <div class="single-elomous-product">
-              <!-- Product Image Start -->
-              <div class="pro-img">
-                <a href="shop/detail">
-                  <img class="primary-img" src="./img/product/tas.png" alt="single-product">
-                </a>
-              </div>
-              <!-- Product Image End -->
-              <!-- Product Content Start -->
-              <div class="pro-content">
-                <div class="pro-info">
-                  <h4><a href="shop/detail">Ransel Rebool Orange</a></h4>
-                  <p><span class="special-price">Rp. 150.000</span></p>
-                  <!-- <del class="old-price">$80.50</del> -->
-                </div>
-              </div>
-              <!-- Product Content End -->
-              <!-- <span class="sticker-sale">-5%</span> -->
-            </div>
-          </div>
-          <!-- Single Service Area End Here -->
-          <!-- Single Service Area Start Here -->
-          <div class="col-lg-3  mb-sm-40">
-            <div class="single-elomous-product">
-              <!-- Product Image Start -->
-              <div class="pro-img">
-                <a href="shop/detail">
-                  <img class="primary-img" src="./img/product/arsenal.png" alt="single-product">
-                </a>
-              </div>
-              <!-- Product Image End -->
-              <!-- Product Content Start -->
-              <div class="pro-content">
-                <div class="pro-info">
-                  <h4><a href="shop/detail">Jersey Puma Arsenal 2018/2019</a></h4>
-                  <p><span class="special-price">Rp. 850.000</span></p>
-                  <!-- <del class="old-price">$80.50</del> -->
-                </div>
-              </div>
-              <!-- Product Content End -->
-              <!-- <span class="sticker-sale">-5%</span> -->
-            </div>
-          </div>
-          <!-- Single Service Area End Here -->
-          <!-- Single Service Area Stat Here -->
-          <div class="col-lg-3 ">
-            <div class="single-elomous-product">
-              <!-- Product Image Start -->
-              <div class="pro-img">
-                <a href="shop/detail">
-                  <img class="primary-img" src="./img/product/vans.png" alt="single-product">
-                </a>
-              </div>
-              <!-- Product Image End -->
-              <!-- Product Content Start -->
-              <div class="pro-content">
-                <div class="pro-info">
-                  <h4><a href="shop/detail">Vans Old Skool Black</a></h4>
-                  <p><span class="special-price">Rp. 500.000</span></p>
-                  <!-- <del class="old-price">$80.50</del> -->
-                </div>
-              </div>
-              <!-- Product Content End -->
-              <!-- <span class="sticker-sale">-5%</span> -->
-            </div>
-          </div>
+        @endforeach
           <!-- Single Service Area End Here -->
         </div>
         <br>
