70d152e45d1a4fb37ff163f0a28adb4d88023574
kevin <kahal.kevin@gmail.com>
2018-11-25 21:18:15 +0700

    cart in header logic for null data

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -162,7 +162,7 @@ class ShopController extends Controller
                 $order->_kecamatan = $defaultAddress->_kecamatan;
                 $order->_kode_pos = $defaultAddress->_kode_pos;
                 $shippingCost = $this->getCost($order->ro_city_id, 'tiki', 1700);
-                dd($shippingCost);
+                // dd($shippingCost);
             }
             $idx = 0;
             $orderItems = array();
@@ -182,7 +182,7 @@ class ShopController extends Controller
                 $orderItems[$idx] = $detail;
                 $idx++;
             }
-            dd($orderItems);
+            // dd($orderItems);
         } else {
             // dd(0);
         }
@@ -205,7 +205,8 @@ class ShopController extends Controller
             "amount" => 10000,
         );
 
-        $token = MidtransHelper::purchase($order_detail);
+        $token = "test";
+        // $token = MidtransHelper::purchase($order_detail);
         
 
         return view('shop.checkout', compact('token'));
@@ -223,7 +224,8 @@ class ShopController extends Controller
             "amount" => 10000,
         );
 
-        $token = MidtransHelper::purchase($order_detail);
+        $token = "test";
+        // $token = MidtransHelper::purchase($order_detail);
         // $orderId = DB::select("select nextseq('fe_tx_order','') FROM DUAL;");
         return view('shop.checkout_guest', compact('token'));
     }
--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,8 @@ Route::prefix('shop')->group(function () {
     Route::get('indexByTag/{id}', 'ShopController@indexByTag');    
     Route::get('detail/{slug}', 'ShopController@detail');
     Route::get('cart', 'ShopController@cart');
-    Route::post('checkout', 'ShopController@checkout');
-    Route::post('checkoutGuest', 'ShopController@checkoutGuest');
+    Route::get('checkout', 'ShopController@checkout');
+    Route::get('checkoutGuest', 'ShopController@checkoutGuest');
     Route::view('shoppingcart', 'shop.cart');
     Route::post('addToCart/{product_id}', 'ShopController@addToCart');
 });
